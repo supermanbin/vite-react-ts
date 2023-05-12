410169676788f922859c7b628b0887ee7d44a94d
fengbin <bin.feng@wiz.ai>
2023-05-12 22:02:12 +0800

    promise

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,19 @@ function App() {
     api.start({
       to: {width: springs.width.get() === 80 ? 200 : 80}
     })
+
   }
+
+  const p = new Promise((resolve, reject) => {
+    resolve('This is resolve');
+  });
+  console.log(p);
+
+  p.then((val) => {
+    console.log(val);
+  }).catch(reason => {
+    console.log(reason);
+  });
   
   
 
@@ -34,7 +46,9 @@ function App() {
         height:80, 
         background: '#ff6d6d', 
         ...springs
-        }} onClick={handleClick}></animated.div>
+        }} onClick={handleClick}>
+          {springs.width.to(x => x.toFixed(0))}
+        </animated.div>
       <form action="">
         <label className='flex flex-row items-center'>
           <input type="radio" name="fruit" className='w-5 h-5 mr-2' defaultChecked />
