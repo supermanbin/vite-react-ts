78d96c2bfa2159f67c8c1c30668953267c3c0055
fengbin <fengbinsuperman@gmail.com>
2023-10-30 16:12:09 +0800

    audio visualization

--- src/components/FileInput/FileInput.tsx
+++ src/components/FileInput/FileInput.tsx
@@ -1,21 +1,29 @@
 import style from './FileInput.module.css';
+import { useRef, useState } from 'react';
 
 type FileInputType = {
   onChange: (file: any) => void;
 };
-export default function FileInput(props: FileInputType) {
-  const { onChange } = props;
-
+export default function FileInput({ onChange }: FileInputType) {
+  const [val, setVal] = useState('');
+  const fileRef = useRef();
   const fileChange = (e: any) => {
     const files = e.target.files;
+
     if (!files.length) return;
+    setVal(e.target.value);
     onChange(files[0]);
+  };
+
+  const handleClick = () => {
+    setVal('');
   };
 
   return (
     <div className={style['file-uploader']}>
       <label htmlFor="fileUploader">请选择文件</label>
-      <input type="file" id="fileUploader" onChange={fileChange} />
+      <input type="file" id="fileUploader" value={val} onChange={fileChange} />
+      <button onClick={handleClick}>Clear</button>
     </div>
   );
 }
--- src/pages/audioCut/AudioCut.tsx
+++ src/pages/audioCut/AudioCut.tsx
@@ -4,44 +4,82 @@ import style from './AudioCut.module.css';
 
 export default function AudioCut() {
   const [fileName, setFileName] = useState('');
-  const [ctx, setCtx] = useState(null);
   const audioRef: MutableRefObject<HTMLAudioElement> = useRef(null);
-  const canvasRef: MutableRefObject<null> = useRef(null);
+  const canvasRef: MutableRefObject<HTMLCanvasElement> = useRef(null);
 
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext('2d');
-    canvas.width = canvas.clientWidth * window.devicePixelRatio;
-    canvas.height = canvas.clientHeight * window.devicePixelRatio;
-
-    setCtx(ctx);
-  }, []);
+  useEffect(() => {}, []);
 
   const fileChange = (file: any) => {
-    const reader = new FileReader();
-    reader.readAsArrayBuffer(file);
-    audioRef.current.src = URL.createObjectURL(file);
-    audioRef.current.play();
-    const audioCtx = new AudioContext();
-    console.log(audioCtx);
-    const analyser = audioCtx.createAnalyser();
-    analyser.fftSize = 1024;
-
-    const mes = audioCtx.createMediaElementSource(audioRef.current);
-    mes.connect(analyser);
-    mes.connect(audioCtx.destination);
-
+    // const reader = new FileReader();
+    const audio = audioRef.current;
+    audio.src = URL.createObjectURL(file);
+    audio.load();
     setFileName(file.name);
+    // 1. 创建音频上下文
+    const audioCtx = new AudioContext();
+    // 2. 创建音频源
+    const audioSource = audioCtx.createMediaElementSource(audio);
+    // 3. 创建音频分析节点
+    const analyser = audioCtx.createAnalyser();
+    // 4. 音频源连接音频节点
+    audioSource.connect(analyser);
+    // 5. 音频节点连接输出设备
+    analyser.connect(audioCtx.destination);
+    // 6. 设置fftSize(快速傅里叶变换)
+    analyser.fftSize = 256;
+    // 7. 设置频率计数，此值一般为fftsize的一半
+    const bufferLength = analyser.frequencyBinCount;
+    console.log(analyser);
+    const dataArray = new Uint8Array(bufferLength);
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+    const WIDTH = (canvas.width = canvas.clientWidth * window.devicePixelRatio);
+    const HEIGHT = (canvas.height = canvas.clientHeight * window.devicePixelRatio);
+    const barW = WIDTH / bufferLength / 2;
+    // const barW = 2;
+    let barH;
+    let x = 0;
+    const gradient = ctx.createLinearGradient(0, 0, WIDTH, HEIGHT);
+    gradient.addColorStop(0, '#6500ff');
+    gradient.addColorStop(0.5, '#ff00a2');
+    gradient.addColorStop(1, '#6500ff');
+    // #ff00a2, #6500ff
+    function renderFrame() {
+      requestAnimationFrame(renderFrame);
+      x = 0;
+      // 获取每一帧的音频频率数据
+      // 实际是通过getByteFrequencyData方法将音频的采集点的数据回填到dataArray里
+      // 这样dataArray每一帧的数据都是不同的，就可以针对数据进行可视化操作
+      analyser.getByteFrequencyData(dataArray);
+      // 清除画布内容
+      ctx.clearRect(0, 0, WIDTH, HEIGHT);
 
-    reader.onload = function (pe: ProgressEvent<FileReader>) {
-      // let dataArray = new Uint8Array(analyser.frequencyBinCount);
-      // console.log(dataArray);
-      //
-      // analyser.getByteFrequencyData(dataArray);
-      // console.log(analyser);
-      // ctx.fillRect(0, 0, 100, 100);
-    };
+      for (var i = 0; i < bufferLength; i++) {
+        barH = dataArray[i];
+        // var r = barH + 50 * (i / bufferLength);
+        // var g = 250 * (i / bufferLength);
+        // var b = 250 * (i / bufferLength);
+        // ctx.fillStyle = 'rgb(' + r + ',' + g + ',' + b + ')';
+        ctx.fillStyle = gradient;
+        // X轴的前半部分数据
+        // ctx.fillRect(WIDTH / 2 - x, HEIGHT - barH, barW, barH);
+        // X轴的后半部分数据
+        // ctx.fillRect(WIDTH / 2 + x, HEIGHT - barH, barW, barH);
+
+        // 镜像操作
+        ctx.fillRect(WIDTH / 2 - x, HEIGHT / 2, barW, -barH / 2);
+        ctx.fillRect(WIDTH / 2 - x, HEIGHT / 2, barW, barH / 2);
+        ctx.fillRect(WIDTH / 2 + x, HEIGHT / 2, barW, -barH / 2);
+        ctx.fillRect(WIDTH / 2 + x, HEIGHT / 2, barW, barH / 2);
+        x += barW;
+      }
+    }
+
+    audio.play();
+    renderFrame();
   };
+
+  const clearFile = () => {};
 
   return (
     <div>
@@ -49,8 +87,8 @@ export default function AudioCut() {
       <audio ref={audioRef} controls>
         <source src="" />
       </audio>
-      <canvas className={style.canvas} ref={canvasRef}></canvas>
       <FileInput onChange={fileChange}></FileInput>
+      <canvas className={style.canvas} ref={canvasRef}></canvas>
     </div>
   );
 }
