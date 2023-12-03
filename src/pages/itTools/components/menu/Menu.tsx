582f4813623c67b206cf0d2856b8f09a9e1db048
fengbin <bin.feng@wiz.ai>
2023-12-03 18:57:44 +0800

    Token Generator

--- src/pages/itTools/components/menu/Menu.tsx
+++ src/pages/itTools/components/menu/Menu.tsx
@@ -3,10 +3,17 @@ import menu from './menu.module.css';
 
 export default function Menu() {
   return (
-  <ul>
-    <li>
-      <NavLink  className="flex px-2 h-11 items-center" to="token-generator"><span className="rounded-full bg-lime-400">T</span>Token Generator</NavLink>
-    </li>
-  </ul>
+    <ul>
+      <li>
+        <NavLink className="flex px-2 h-11 items-center hover:bg-gray-200" to="token-generator">
+          <span className="rounded-full bg-lime-400 w-6 h-6 text-center mr-3 text-sm">T</span>Token Generator
+        </NavLink>
+      </li>
+      <li>
+        <NavLink className="flex px-2 h-11 items-center hover:bg-gray-200" to="token-generator">
+          <span className="rounded-full bg-lime-400 w-6 h-6 text-center mr-3">H</span>Hash Text
+        </NavLink>
+      </li>
+    </ul>
   );
 }
